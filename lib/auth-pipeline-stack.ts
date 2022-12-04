a37558e263fbb2aebb6723afe6a02e9e78eb5774
Jongchan Ko <godol811@naver.com>
2022-12-04 17:45:43 +0900

    refactor(Network LoadBalancer 체크): Network LoadBalancer 포트 확인용

--- lib/auth-pipeline-stack.ts
+++ lib/auth-pipeline-stack.ts
@@ -180,7 +180,7 @@ export class AuthPipelineStack extends cdk.Stack {
                             actionName: 'CFN_Deploy',
                             stackName: 'EcsStackDeployedInPipeline',
                             // this name has to be the same name as used below in the CDK code for the application Stack
-                            templatePath: cdkCodeBuildOutput.atPath('EcsPipelineStack.template.json'),
+                            templatePath: cdkCodeBuildOutput.atPath('EcsStackDeployedInPipeline.template.json'),
                             adminPermissions: true,
                             parameterOverrides: {
                                 // read the tag pushed to the ECR repository from the CodePipeline Variable saved by the application build step,
