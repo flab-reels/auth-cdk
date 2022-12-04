c6e6441c581f0c1788b2fd0436cec56d5a40dba4
Jongchan Ko <godol811@naver.com>
2022-12-04 20:18:38 +0900

    refactor(Network LoadBalancer 체크): Network LoadBalancer 포트 확인용

--- lib/auth-pipeline-stack.ts
+++ lib/auth-pipeline-stack.ts
@@ -108,12 +108,14 @@ export class AuthPipelineStack extends cdk.Stack {
                     install: {
                         commands: [
                             'npm install',
+                            'npm install -g aws-cdk',
                             "n 16.15.1",
                         ],
                     },
                     build: {
                         commands: [
                             // synthesize the CDK code for the ECS application Stack
+                            'npx cdk --version',
                             'npx cdk synth --verbose',
                         ],
                     },
